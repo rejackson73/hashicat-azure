17b7d511e8f902aa8c5cd6f4ca03bcd72324373b
Rob Jackson <28791872+rejackson73@users.noreply.github.com>
2019-12-10 16:57:26 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,27 +3,12 @@ resource "azurerm_resource_group" "myresourcegroup" {
   location = "${var.location}"
 }
 
-locals {
-  service_name = "forum"
-  owner        = "Community Team"
-}
-
-locals {
-  # Common tags to be assigned to all resources
-  common_tags = {
-    Service = local.service_name
-    Owner   = local.owner
-  }
-}
 
 resource "azurerm_virtual_network" "vnet" {
   name                = "${var.prefix}-vnet"
   location            = "${azurerm_resource_group.myresourcegroup.location}"
   address_space       = ["${var.address_space}"]
   resource_group_name = "${azurerm_resource_group.myresourcegroup.name}"
-  tags = {
-    Owner = ${local.common_tags.Owner}
-    }
 }
 
 resource "azurerm_subnet" "subnet" {
