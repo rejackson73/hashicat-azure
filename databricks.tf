e347fbcf06e1b1888fdce86a340578872e232ec6
rejackson73 <28791872+rejackson73@users.noreply.github.com>
2020-01-09 13:59:49 -0500

    Revert "Update databricks.tf"
    This reverts commit 40946fbd35ce9bcc015420e5b33684ffd8b9888e.

--- databricks.tf
+++ databricks.tf
@@ -1,7 +1,7 @@
 resource "azurerm_databricks_workspace" "example" {
   name                = "databricks-test"
-  resource_group_name = "${data.azurerm_resource_group.myresourcegroup.name}"
-  location            = "${data.azurerm_resource_group.myresourcegroup.location}"
+  resource_group_name = "${azurerm_resource_group.myresourcegroup.name}"
+  location            = "${azurerm_resource_group.myresourcegroup.location}"
   sku                 = "standard"
 
   tags = {
